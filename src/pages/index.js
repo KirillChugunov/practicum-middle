e16cd5bc5c3ac89504f910eb4076aedc5e04e7da
Kirill Chugunov <chugunov.front@gmail.com>
2024-11-14 22:34:31 +0300

    sprint_1

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ export {LoginPage} from './loginPage'
 export {SingInPage} from './singInPage'
 export {UserProfileInfoPage} from './userProfileInfoPage'
 export { UserProfilePasswordEditPage } from './userProfilePasswordEditPage';
-export { UserProfileEditPage } from './UserProfileEditPage'
+export { UserProfileEditPage } from './userProfileEditPage'
 export { ChatListPage } from './chatList'
 export {NotFoundPage } from './404'
 export {ErrorPage} from './5xx'
